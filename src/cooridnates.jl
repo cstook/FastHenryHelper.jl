3a11f82fd7aa20493d57f4995c1d48422c629f19
Chris Stook <chris@chrisstook.com>
2016-05-03 00:14:33 -0400

    Signed-off-by: Chris Stook <chris@chrisstook.com>

--- src/cooridnates.jl
+++ src/cooridnates.jl
@@ -2,7 +2,10 @@
 
 abstract Point
 
-"a point or vector in [Spherical](http://mathworld.wolfram.com/SphericalCoordinates.html) cooridnates"
+"""
+a point or vector in [Spherical](http://mathworld.wolfram.com/SphericalCoordinates.html) cooridnates
+Spherical(r,θ,φ)
+"""
 type Spherical <: Point
   "radial"
   r :: Float64 # radial
@@ -12,7 +15,10 @@ type Spherical <: Point
   φ :: Float64 # polar
 end
 
-"a point or vector in [Cartesian](http://mathworld.wolfram.com/CartesianCoordinates.html) cooridnates"
+"""
+a point or vector in [Cartesian](http://mathworld.wolfram.com/CartesianCoordinates.html) cooridnates
+Polar(r,θ,z)
+"""
 type Polar <: Point
   "radial"
   r :: Float64 # radial
@@ -22,7 +28,10 @@ type Polar <: Point
   z :: Float64
 end
 
-"a point or vector in [Cylindrical](http://mathworld.wolfram.com/CylindricalCoordinates.html) cooridnates"
+"""
+a point or vector in [Cylindrical](http://mathworld.wolfram.com/CylindricalCoordinates.html) cooridnates
+Cartesian(x,y,z)
+"""
 type Cartesian <: Point
   x :: Float64
   y :: Float64
@@ -57,6 +66,14 @@ function Base.convert(::Type{Cartesian}, arg::Polar)
   return Cartesian(x,y,z)
 end
 
+function Base.convert{T<:Type{Point}}(::T, arg::Array{Point})
+  result = Array(T,size(arg))
+  for i in eachindex(arg)
+    result[i] = convert(T,arg[i])
+  end
+  return result
+end
+
 function translate(point::Cartesian, vector::Cartesian)
   x = point.x + vector.x
   y = point.y + vector.y 
@@ -71,8 +88,69 @@ function translate!(point::Cartesian, vector::Cartesian)
   return nothing
 end
 
-function translate{T::Point}(point::T, vector::Point)
+function translate{T<:Point}(point::T, vector::Point)
   pc = convert(Cartesian,point)
   vc = convert(Cartesian,vector)
   convert(T,translate(pc,vc))
 end
+
+function translate!(point::Point, vector::Point)
+  point = translate(point,vector)
+  return nothing
+end
+
+function translate!(points::Array{Point}, vector::Point)
+  for i in eachindex(points)
+    translate!(points[i],vector)
+  end
+  return nothing
+end
+
+"""
+    translate(point::Point, vector::Point)
+    translate!(point::Point, vector::Point)
+    translate!(points::Array{Point}, vector::Point)
+
+translates `point` or `points` by `vector`
+"""
+translate, translate!
+
+function rotate(point::Spherical, vector::Spherical)
+  θ = point.θ + vector.θ
+  φ = point.φ + vector.φ
+  return Spherical(point.r,θ,φ)
+end
+
+function rotate!(point::Spherical, vector::Spherical)
+  point.θ += vector.θ
+  point.φ += vector.φ
+  return nothing
+end
+
+function rotate{T<:Point}(point::T, vector::Point)
+  ps = convert(Spherical,point)
+  vs = convert(Spherical,vector)
+  convert(T,rotate(ps,vs))
+end
+
+function rotate!(point::Point, vector::Point)
+  point = rotate(point,vector)
+  return nothing
+end
+
+function rotate!(points::Array{Point}, vector::Point)
+  for i in eachindex(points)
+    rotate!(points[i],vector)
+  end
+  return nothing
+end
+
+"""
+    rotate(point::Point, vector::Point)
+    rotate!(point::Point, vector::Point)
+    rotate!(points::Array{Point}, vector::Point)
+
+rotates `point` or `points` by `vector`
+"""
+rotate, rotate!
+
