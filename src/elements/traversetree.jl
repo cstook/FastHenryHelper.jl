4be9b346fbdfb19fcbfa24873c86c1a3e3844a6a
Chris Stook <chris@chrisstook.com>
2016-07-25 08:20:52 -0400

    fixed ttaverse tree to handle nested groups properly
    Signed-off-by: Chris Stook <chris@chrisstook.com>

--- src/elements/traversetree.jl
+++ src/elements/traversetree.jl
@@ -3,21 +3,23 @@
 # might be more efficent with IObuffer instead of shifting and
 # prepending an array
 
+# state is an array of elements to be retruned
 Base.start(e::Element) = [e]
 Base.start(e::Group) = copy(e.elements)
+
+function Base.next(e::Element, state)
+  updatestate!(state, state[1]) # in case element is a group
+  item = shift!(state)
+  return (item,state)
+end
+
+Base.done(e::Element, state) = length(state) == 0
 
 updatestate!(state, s::Element) = nothing
 function updatestate!(state, s::Group)
   # replace group with its elements
   shift!(state)
   prepend!(state,s.elements)
+  updatestate!(state, state[1])
   return nothing
 end
-
-function Base.next(e::Element, state)
-  updatestate!(state, state[1])
-  item = shift!(state)
-  return (item,state)
-end
-
-Base.done(e::Element, state) = length(state) == 0
