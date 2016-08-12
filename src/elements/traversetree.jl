6ad1d1f5206b7c8f51dd59252d805be8ddafe25f
Chris Stook <chris@chrisstook.com>
2016-08-12 16:47:38 -0400

    Signed-off-by: Chris Stook <chris@chrisstook.com>

--- src/elements/traversetree.jl
+++ src/elements/traversetree.jl
@@ -4,18 +4,43 @@ type TTState
   togo :: Array{Element,1}
   units :: Units
 end
-
-Base.start(element::Element) = TTState([e],Units())
-function Base.Start(group::Group)
-  if group.units == Units("")
-    return TTState([copy(group.element)],group.units)
+update!(state::TTState, state1::Element) = nothing
+function update!(state::TTState, state1::Group)
+  # replace group with its elements.
+  # must pass first element of state.togo as second parameter
+  # for proper dispach
+  shift!(state.togo)
+  if state1.units == Units()
+    prepend!(state.togo, state1.elements)
   else
-    return TTState([group.units;copy(group.element)],groupunits)
+    unshift!(state.togo, state.units)
+    prepend!(state.togo, state1.elements)
+    unshift!(state.togo, state1.units)
+  end
+  update!(state, state.togo[1])
 end
 
+Base.start(element::Element) = TTState([element],Units())
+function Base.start(group::Group)
+  if group.units == Units()
+    return TTState(copy(group.elements),group.units)
+  else
+    return TTState([group.units;copy(group.elements)],groupunits)
+  end
+end
+function Base.next(element::Element, state::TTState)
+  update!(state, state.togo[1]) # in case element is a group
+  returnelement = shift!(state.togo)
+  return (returnelement, state)
+end
+Base.done(element::Element, state::TTState) = length(state.togo) == 0
 
 
 
+
+#=
+
+# old version #############
 # state is an array of elements to be retruned
 Base.start(e::Element) = [e]
 Base.start(e::Group) = copy(e.elements)
@@ -36,3 +61,4 @@ function updatestate!(state, s::Group)
   updatestate!(state, state[1])
   return nothing
 end
+=#
