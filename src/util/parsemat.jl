27fa8f41c1774bc2e4fb53e30e0ac2c72dbbb956
Chris Stook <chris@chrisstook.com>
2018-09-22 13:49:26 -0400

    0.7 -> 1.0

--- src/util/parsemat.jl
+++ src/util/parsemat.jl
@@ -24,7 +24,7 @@ mutable struct ParseFastHenryMatResult
   impedance matrix at each frequency.  <br>
   impedance [impedance matrix row : impedance matrix column : frequency]
   """
-  impedance :: Array{Complex{Float64},3}
+  impedance :: Array{ComplexF64,3}
 end
 
 function parsefasthenrymat(io::IO)
@@ -32,29 +32,29 @@ function parsefasthenrymat(io::IO)
   line = readline(io)
   m = match(findportname,line)
   if m==nothing
-    throw(ParseError("first line did not start with \"Row\""))
+    throw(Meta.ParseError("first line did not start with \"Row\""))
   end
   numberofrows = parse(Int,m.captures[1])
-  portnames = Array{AbstractString}(undef,numberofrows)
+  portnames = Array{String}(undef,numberofrows)
   portnames[numberofrows] = m.captures[3] == nothing ? "" : m.captures[3]
   for i in numberofrows-1:-1:1
     line = readline(io)
     m = match(findportname,line)
     if m==nothing
       rowstring = @sprintf("%d",i)
-      throw(ParseError("did not find rows line for Row="*rowstring))
+      throw(Meta.ParseError("did not find rows line for Row="*rowstring))
     end
     portnames[i] = m.captures[3] == nothing ? "" : m.captures[3]
   end
   frequencies = 0
   buffer = IOBuffer()
-  impeadancematrix = Array{Complex{Float64}}(undef,numberofrows,numberofrows)
+  impeadancematrix = Array{ComplexF64,2}(undef,numberofrows,numberofrows)
   while ~eof(io)
     line = readline(io)
     frequencies+=1
     m = match(r"Impedance matrix for frequency = ([\d+-eE.]+) ",line)
     if m==nothing
-      throw(ParseError("did not find \"Impedance matrix for frequency = \""))
+      throw(Meta.ParseError("did not find \"Impedance matrix for frequency = \""))
     end
     frequency = parse(Float64,m.captures[1])
     write(buffer,frequency)
@@ -65,17 +65,17 @@ function parsefasthenrymat(io::IO)
       for column in 1:numberofrows # impedance matrix is square
         m = match(r"([\d+-.eE]+) *([\d+-.eE]+)j",line,position)
         if m==nothing
-          throw(ParseError("error parsing impedance matrix"))
+          throw(Meta.ParseError("error parsing impedance matrix"))
         end
         r = parse(Float64,m.captures[1])
         i = parse(Float64,m.captures[2])
-        impeadancematrix[row,column]=Complex64(r,i)
+        impeadancematrix[row,column]=ComplexF64(r,i)
         position = m.offset + length(m.match)
       end
     end
     write(buffer,impeadancematrix)
   end
-  resultmatrix = Array{Complex{Float64}}(undef,numberofrows,numberofrows,frequencies)
+  resultmatrix = Array{ComplexF64,3}(undef,numberofrows,numberofrows,frequencies)
   frequencymatrix = Array{Float64}(undef,frequencies)
   seekstart(buffer)
   for i in 1:frequencies
@@ -87,7 +87,7 @@ function parsefasthenrymat(io::IO)
   ParseFastHenryMatResult(portnames,frequencymatrix,resultmatrix)
 end
 
-function parsefasthenrymat(filename::AbstractString)
+function parsefasthenrymat(filename::String)
   io = open(filename,"r")
   result = parsefasthenrymat(io)
   close(io)
